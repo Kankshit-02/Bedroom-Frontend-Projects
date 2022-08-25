9f59d5f8ed84df554138395fee7cafd5364103cc
KankshitM <kankshitm2002@gmail.com>
2022-08-26 01:11:16 +0530

    increment-decrement push

---
+++ Increment-Decrement/index.js
@@ -0,0 +1,17 @@
+
+
+
+function decrement(){
+    let counter=parseInt(document.getElementById("number").value,10)
+    counter -= 1
+    document.getElementById("number").value = counter;
+    
+}
+
+function increment(){
+    
+    let counter=parseInt(document.getElementById("number").value,10)
+    counter +=1
+    document.getElementById("number").value = counter;
+}
+
